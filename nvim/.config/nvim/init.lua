d446add5b6d27dfd8dbfc264b284f3abe0095baa
Jean-Christophe Pennel <jpennel@amadeus.com>
2022-09-03 10:08:02 -0400

    init.lua imports refactored

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,20 @@
-require "jcp.options"
-require "jcp.keymaps"
-require "jcp.plugins"
-require "jcp.colorscheme"
-require "jcp.cmp"
-require "jcp.lsp"
-require "jcp.telescope"
-require "jcp.treesitter"
-require "jcp.autopairs"
-require "jcp.comment"
-require "jcp.nvim-tree"
-require "jcp.bufferline"
-require "jcp.lualine"
-require "jcp.impatient"
-require "jcp.indentline"
-require "jcp.alpha"
-require "jcp.whichkey"
-require "jcp.autocommands"
+require("jcp.options")
+require("jcp.keymaps")
+require("jcp.plugins")
+require("jcp.colorscheme")
+require("jcp.cmp")
+require("jcp.lsp")
+require("jcp.telescope")
+require("jcp.treesitter")
+require("jcp.autopairs")
+require("jcp.comment")
+require("jcp.nvim-tree")
+require("jcp.bufferline")
 require("jcp.gitsigns")
+require("jcp.lualine")
+require("jcp.impatient")
+require("jcp.indentline")
+require("jcp.alpha")
+require("jcp.whichkey")
+require("jcp.autocommands")
 require("jcp.toggleterm")
