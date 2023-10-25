a9e8516c707370cb34915d8534bef6b1604626f7
JC Pennel <jcpennel@gmail.com>
2023-10-24 20:00:20 -0400

    Polybar: added 2nd bar, temperature module and power menu

--- polybar/.config/polybar/polybar_launch.sh
+++ polybar/.config/polybar/polybar_launch.sh
@@ -7,7 +7,15 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch top bar
-echo "---" | tee -a /tmp/polybar1.log #/tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log #/tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
 
 echo "Bar(s) launched..."
