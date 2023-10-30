816627572c7e13eb19e4d8dc4c55c3fabf921ca0
Jean-Christophe Pennel <jpennel@amadeus.com>
2023-10-30 10:33:58 -0400

    Added a simple Goyo config in vimwiki file

--- nvim/.config/nvim/lua/jcp/vimwiki.lua
+++ nvim/.config/nvim/lua/jcp/vimwiki.lua
@@ -9,4 +9,29 @@ let g:vimwiki_global_ext = 0
 
 " Not sure this one is actually necessary
 let g:vimwiki_ext2syntax = {'.md': 'markdown'}
+
+let g:goyo_width = "70%"
+
+" Goyo configuration
+function! s:goyo_enter()
+  silent !tmux set status off
+  silent !tmux list-panes -F '\#F' | grep -q Z || tmux resize-pane -Z
+  set noshowmode
+  set noshowcmd
+  set scrolloff=999
+  " ...
+endfunction
+
+function! s:goyo_leave()
+  silent !tmux set status on
+  silent !tmux list-panes -F '\#F' | grep -q Z && tmux resize-pane -Z
+  set showmode
+  set showcmd
+  set scrolloff=5
+  " ...
+endfunction
+
+autocmd! User GoyoEnter nested call <SID>goyo_enter()
+autocmd! User GoyoLeave nested call <SID>goyo_leave()
+
 ]])
