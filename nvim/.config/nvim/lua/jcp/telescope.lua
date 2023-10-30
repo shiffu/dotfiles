37b4c09dd6625471ba78b9b8b2144158e4853c1e
Jean-Christophe Pennel <jpennel@amadeus.com>
2023-10-30 10:33:39 -0400

    Updated telescope config to allow searching (both file and grep) in hidden folders (except .git folder)

--- nvim/.config/nvim/lua/jcp/telescope.lua
+++ nvim/.config/nvim/lua/jcp/telescope.lua
@@ -3,10 +3,27 @@ if not status_ok then
   return
 end
 
+local status_ok, telescope_config = pcall(require, "telescope.config")
+if not status_ok then
+  return
+end
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
 local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
+
+    -- `hidden = true` is not supported in text grep commands.
+	vimgrep_arguments = vimgrep_arguments,
 
     prompt_prefix = " ",
     selection_caret = " ",
@@ -85,6 +102,11 @@ telescope.setup {
     -- }
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
+
+    find_files = {
+        -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
   },
   extensions = {
     -- Your extension configuration goes here:
