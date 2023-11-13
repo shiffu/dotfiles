5063e516954b382ac7522e266db0398781ec5416
JC Pennel <jcpennel@gmail.com>
2023-11-12 22:48:32 -0500

    Updated vimwiki path

--- nvim/.config/nvim/lua/jcp/vimwiki.lua
+++ nvim/.config/nvim/lua/jcp/vimwiki.lua
@@ -1,5 +1,5 @@
 vim.cmd([[
-let g:vimwiki_list = [{'path': '~/MySynologyDrive/doc/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
+let g:vimwiki_list = [{'path': '~/SynologyDrive/doc/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
 
 " Makes vimwiki markdown links as [text](text.md) instead of [text](text)
 let g:vimwiki_markdown_link_ext = 1
