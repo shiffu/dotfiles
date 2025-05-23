0b25a9ab586bc7cde638f58c86fe4cb120421a07
JC Pennel <jcpennel@gmail.com>
2025-05-22 21:56:49 -0400

    Added first Wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,92 @@
+local appearance = require 'appearance'
+
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Appearance
+-- config.color_scheme = 'Arthur'
+-- config.color_scheme = 'Catppuccin Mocha'
+-- config.color_scheme = 'Dracula'
+-- config.color_scheme = 'hardhacker'
+-- config.color_scheme = 'Tokyo Night Storm'
+-- config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Everforest Dark - Hard'
+
+-- Term
+config.term = 'xterm-256color'
+-- config.term = 'wezterm'
+
+-- Window
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+
+-- Font
+config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular' })
+config.font_size = 11
+
+local function segments_for_right_status(window)
+  return {
+    wezterm.strftime('%a %b %-d %H:%M'),
+    wezterm.hostname(),
+  }
+end
+
+wezterm.on('update-status', function(window, _)
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  local segments = segments_for_right_status(window)
+
+  local color_scheme = window:effective_config().resolved_palette
+  -- Note the use of wezterm.color.parse here, this returns
+  -- a Color object, which comes with functionality for lightening
+  -- or darkening the colour (amongst other things).
+  local bg = wezterm.color.parse(color_scheme.background)
+  local fg = color_scheme.foreground
+
+  -- Each powerline segment is going to be coloured progressively
+  -- darker/lighter depending on whether we're on a dark/light colour
+  -- scheme. Let's establish the "from" and "to" bounds of our gradient.
+  local gradient_to, gradient_from = bg
+  if appearance.is_dark() then
+    gradient_from = gradient_to:lighten(0.2)
+  else
+    gradient_from = gradient_to:darken(0.2)
+  end
+
+  -- Yes, WezTerm supports creating gradients, because why not?! Although
+  -- they'd usually be used for setting high fidelity gradients on your terminal's
+  -- background, we'll use them here to give us a sample of the powerline segment
+  -- colours we need.
+  local gradient = wezterm.color.gradient(
+    {
+      orientation = 'Horizontal',
+      colors = { gradient_from, gradient_to },
+    },
+    #segments -- only gives us as many colours as we have segments.
+  )
+
+  -- We'll build up the elements to send to wezterm.format in this table.
+  local elements = {}
+
+  for i, seg in ipairs(segments) do
+    local is_first = i == 1
+
+    if is_first then
+      table.insert(elements, { Background = { Color = 'none' } })
+    end
+    table.insert(elements, { Foreground = { Color = gradient[i] } })
+    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+    table.insert(elements, { Foreground = { Color = fg } })
+    table.insert(elements, { Background = { Color = gradient[i] } })
+    table.insert(elements, { Text = ' ' .. seg .. ' ' })
+  end
+
+  window:set_right_status(wezterm.format(elements))
+end)
+
+-- Return the configuration
+return config
