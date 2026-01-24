45283ff368e070e3fd5180acbc80526ffde8938f
jcpennel <jcpennel@gmail.com>
2026-01-24 20:48:46 +0000

    Updated minimal nvim config

--- nvim/.config/nvim/minimal.lua
+++ nvim/.config/nvim/minimal.lua
@@ -1,41 +1,58 @@
 -- Options
 local options = {
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  -- cmdheight = 2,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hlsearch = false,                         -- highlight all matches on previous search pattern
-  incsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  -- showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- no swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-  tabstop = 4,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  -- cursorcolumn = true,
+
+  -- Basic settings
   number = true,                           -- set numbered lines
   relativenumber = true,                   -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 4 {default 4}
-  -- signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = true,                             -- wrap lines ?
-  scrolloff = 5,                           -- keep 5 lines of buffer on top/bottom while scrolling
+  scrolloff = 4,                           -- keep 5 lines of buffer on top/bottom while scrolling
   sidescrolloff = 8,
-  -- guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  cmdheight = 1,                           -- space in the neovim command line for displaying messages
+  -- showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+
+  -- Indentation
+  tabstop = 4,                             -- insert 4 spaces for a tab
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+  expandtab = true,                        -- convert tabs to spaces
+  smartindent = true,                      -- make indenting smarter again
+
+  -- Search
+  hlsearch = false,                        -- highlight all matches on previous search pattern
+  incsearch = true,                        -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  smartcase = true,                        -- smart case
+
+  -- Visual
+  -- cursorcolumn = true,
+  -- guifont = "monospace:h17",            -- the font used in graphical neovim applications
+  cursorline = true,                       -- highlight the current line
+  showtabline = 2,                         -- always show tabs
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  colorcolumn = "120",
+  termguicolors = true,                    -- Better colors for colorschemes supporting it (24bits colors)
+  pumheight = 10,                          -- pop up menu height
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  wrap = true,                             -- wrap lines ?
+
+  -- Completion
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  updatetime = 300,                        -- faster completion (4000ms default)
+
+  -- Behavior
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+
+  -- File Explorer
+
+  -- File handling
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  backup = false,                          -- creates a backup file
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  undofile = true,                         -- enable persistent undo
+  swapfile = false,                        -- no swapfile
 }
 
 vim.opt.shortmess:append "c"
@@ -85,11 +102,6 @@ vim.cmd [[
   augroup end
 ]]
 
--- Colorscheme
-vim.cmd.colorscheme "default"
--- vim.cmd.colorscheme "retrobox"
--- vim.cmd.colorscheme "unokai"
-
 -- Keymaps
 local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
@@ -101,7 +113,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Various Pluging keymaps
-keymap("n", "<Leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<Leader>e", ":Vex<CR>", opts)
 keymap("n", "<Leader>c", ":bd<CR>", opts)
 
 -- Better window navigation
@@ -110,11 +122,13 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- Resize Windows
-keymap("n", "<A-j>", ":resize -10<CR>", opts)
-keymap("n", "<A-k>", ":resize +10<CR>", opts)
-keymap("n", "<A-h>", ":vertical resize -10<CR>", opts)
-keymap("n", "<A-l>", ":vertical resize +10<CR>", opts)
+-- Split & Resize Panes
+keymap("n", "<Leader>v", ":vsplit<CR>", opts)
+keymap("n", "<Leader>s", ":split<CR>", opts)
+keymap("n", "<A-j>", ":resize -3<CR>", opts)
+keymap("n", "<A-k>", ":resize +3<CR>", opts)
+keymap("n", "<A-h>", ":vertical resize -3<CR>", opts)
+keymap("n", "<A-l>", ":vertical resize +3<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<C-Right>", ":bnext<CR>", opts)
@@ -130,4 +144,12 @@ keymap("i", "jk", "<ESC>", opts)
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
+
+-- Quick edit the nvim config
+keymap("n", "<Leader>rc", ":e ~/.config/nvim/init.lua<CR>", opts)
+
+-- Colorscheme
+-- vim.cmd.colorscheme "default"
+vim.cmd.colorscheme "habamax"
+-- vim.cmd.colorscheme "unokai"
 
