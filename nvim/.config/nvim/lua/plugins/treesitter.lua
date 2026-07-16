e0f2f57f5ec3847a3c7ba22aef477d86720382c0
JC Pennel <jcpennel@gmail.com>
2026-07-16 15:55:51 -0400

    Added toggleterm, gitsigns and updated treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  config = function()
-    local filetypes = {'c', 'lua', 'vim',  'luadoc', 'vimdoc', 'json', 'jsonnet', 'yaml', 'xml', 'go', 'python',
-                       'markdown', 'markdown_inline', 'helm', 'html', 'http', 'jinja', 'kcl', 'tcl'}
+  build = function()
+    local filetypes = {'bash', 'c', 'lua', 'vim',  'luadoc', 'vimdoc', 'json', 'jsonnet', 'yaml', 'xml', 'go',
+                       'python', 'markdown', 'markdown_inline', 'helm', 'html', 'http', 'jinja', 'kcl', 'tcl'}
 
     require('nvim-treesitter').install(filetypes)
     vim.api.nvim_create_autocmd('FileType', {
